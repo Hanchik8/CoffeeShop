2defb5711e92e170f5ff32af3310c408e28d1547
Azizkhan <azizkhan.nurlanov@alatoo.edu.kg>
2025-11-26 22:36:32 +0600

    Second commit

--- src/main/java/org/example/coffeeshop/repository/MenuItemRepository.java
+++ src/main/java/org/example/coffeeshop/repository/MenuItemRepository.java
@@ -2,6 +2,7 @@ package org.example.coffeeshop.repository;
 import org.example.coffeeshop.model.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
     List<MenuItem> findByCategoryIdOrderByNameAsc(Long categoryId);
 }
